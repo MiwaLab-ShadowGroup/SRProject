93f31643bb72d1504409f4db1c926aa51d04f8dd
YusukeKajita <kajita1002@gmail.com>
2016-03-14 19:45:53 +0900

    opencvsharp追加，ウェブカメラ使用の部分など

---
+++ SRProject/Assets/Miwalab/ShadowGroup/Scripts/Sensors/MatAttacher.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MatAttacher : MonoBehaviour {
+
+    public ASensorImporter importer;
+    public Texture2D tex;
+    public Vector2 textureSize;
+    public Material material;
+	// Use this for initialization
+	void Start () {
+	    if(importer == null)
+        {
+            Debug.Log("CameraImporter is not attached.");
+            return;
+        }
+        tex = new Texture2D((int)textureSize.x, (int)textureSize.y);
+        material = GetComponent<Renderer>().material;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (importer == null)
+        {
+            return;
+        }
+        if(importer.getCvMat() == null)
+        {
+            return;
+        }
+        byte[] data = new byte[0];
+        try {
+            data = importer.getCvMat().ToBytes(".PNG");
+        }
+        catch
+        {
+            Debug.Log("trying now...");
+        }
+        tex.LoadImage(data);
+        material.mainTexture = tex;
+    }
+}
---
+++ SRProject/Assets/Miwalab/ShadowGroup/Scripts/Sensors/abstract/ASensorImporter.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+using OpenCvSharp;
+using OpenCvSharp.CPlusPlus;
+
+public abstract class ASensorImporter : MonoBehaviour
+{
+    public abstract Mat getCvMat();
+}
+
