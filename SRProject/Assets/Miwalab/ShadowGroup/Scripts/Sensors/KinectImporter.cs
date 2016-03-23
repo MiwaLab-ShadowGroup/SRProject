b056bb44df30d57ae6bb2bad75264b35aea31e1c
YusukeKajita <kajita1002@gmail.com>
2016-03-23 16:23:39 +0900

    add kinect sdk and tools

---
+++ SRProject/Assets/Miwalab/ShadowGroup/Scripts/Sensors/KinectImporter.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using Windows.Kinect;
+
+public class KinectImporter : MonoBehaviour
+{
+    KinectSensor m_sensor;
+    // Use this for initialization
+    void Start()
+    {
+        m_sensor = KinectSensor.GetDefault();
+        if (m_sensor != null)
+        {
+            Debug.Log("The Kinect ID : " + m_sensor.UniqueKinectId);
+            m_sensor.Open();
+        }
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
