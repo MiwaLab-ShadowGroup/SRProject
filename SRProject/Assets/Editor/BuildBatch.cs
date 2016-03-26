d988da144e23e3223671a850fc1c16894b0482b9
YusukeKajita <kajita1002@gmail.com>
2016-03-26 20:46:11 +0900

    BuildBatch.csの追加

---
+++ SRProject/Assets/Editor/BuildBatch.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System;
+
+public class BuildBatch : MonoBehaviour
+{
+    private static void BuildNull()
+    {
+        string[] scenePaths = {
+      "Assets/Scenes/null.unity"
+    };
+        string outputPath = Application.dataPath + "/../../out.exe";  // dataPathはAssetを指している
+        BuildTarget target = BuildTarget.StandaloneWindows64;
+        BuildOptions opt = BuildOptions.None;
+        string error = BuildPipeline.BuildPlayer(scenePaths, outputPath, target, opt);
+        if (!string.IsNullOrEmpty(error))
+            Debug.LogError(error);
+        EditorApplication.Exit(string.IsNullOrEmpty(error) ? 0 : 1);
+    }
+}
