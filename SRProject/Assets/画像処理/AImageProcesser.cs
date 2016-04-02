d3a046d8f9b71a8e84646cd2a70b8acc3594aed2
Yyamakawa <yy816-b.t.a@ruri.waseda.jp>
2016-04-02 20:27:36 +0900

    画処理追加

---
+++ SRProject/Assets/画像処理/AImageProcesser.cs
@@ -0,0 +1,50 @@
+﻿using OpenCvSharp.CPlusPlus;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace LabLife.Processer.ImageProcesser
+{
+    public enum ParameterType
+    {
+        Float,
+        Double,
+        Int,
+        UInt,
+        Byte,
+        String,
+        Other
+    }
+    public class Parameter
+    {
+        public Parameter(ParameterType Type, Object Value)
+        {
+            this.Type = Type;
+            this.Value = Value;
+        }
+        public ParameterType Type { set; get; }
+        public Object Value { set; get; }
+    }
+
+    public abstract class AImageProcesser
+    {
+        public abstract void ImageProcess(ref Mat src, ref Mat dst);
+        protected AImageProcesser()
+        {
+            m_parameter = new List<Parameter>();
+
+        }
+        /// <summary>
+        /// UI使用するパラメータ取得
+        /// </summary>
+        /// <returns></returns>
+        public List<Parameter> getParameter()
+        {
+            return this.m_parameter;
+        }
+        private List<Parameter> m_parameter;
+    }
+}
