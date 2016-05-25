89591ead3071dadcc2129e2deb76fdf72dd2d608
Yyamakawa <yy816-b.t.a@ruri.waseda.jp>
2016-05-25 20:40:30 +0900

    コミット

--- SRProject/Assets/Miwalab/ShadowGroup/Scripts/Archive/SaveData.cs
+++ SRProject/Assets/Miwalab/ShadowGroup/Scripts/Archive/SaveData.cs
@@ -4,10 +4,11 @@ using System.IO;
 using System.Threading;
 //using UnityEditor;
 using System;
-
-
+using UnityEngine.UI;
+using OpenCvSharp.CPlusPlus;
 [RequireComponent(typeof(KinectImporter))]
-public class SaveData : MonoBehaviour {
+public class SaveData : MonoBehaviour
+{
 
     BinaryWriter writer;
 
@@ -17,39 +18,44 @@ public class SaveData : MonoBehaviour {
 
     //CameraImporter camera;
 
+
+    string FolderPath;
+
     FPSAdjuster.FPSAdjuster FpsAd;
 
     bool OpenFileChoose = false;
 
-    //bool Savestart = false;
+    bool Savestart = false;
 
     bool IsSaveStop = false;
 
-    string Filename = "";
+    string filename;
 
     DateTime datetime;
     TimeSpan timestump;
-    //private Text m_SaveNameTextUI;
+    private Text m_SaveNameTextUI;
 
     ushort[] _savedDepthBuffer;
 
     private void SaveStart_Clicked(object sender, EventArgs e)
     {
-        if(Filename != null)
+        if (FolderPath != null)
         {
-            this.writer = new BinaryWriter(File.OpenWrite(Filename));
+            this.writer = new BinaryWriter(File.OpenWrite(FolderPath + @"\" + this.m_SaveNameTextUI.text));
             thread = new Thread(new ThreadStart(SaveDepth));
             thread.Start();
         }
-       
+
     }
 
-    
+
+
     // Use this for initialization
-    void Start () {
+    void Start()
+    {
         kinect = gameObject.GetComponent<KinectImporter>();
         //this.pointcloud = pointCloudShadow.GetComponent<PointCloud>();
-        (UIHost.GetUI("ChooseSaveFile") as ParameterButton).Clicked += ChooseFile_Clicked;
+        (UIHost.GetUI("ChooseSaveFile") as ParameterButton).Clicked += ChooseFolder_Clicked;
         //this.m_SaveNameTextUI = (UIHost.GetUI("SaveFileName") as ParameterText).m_valueText;
 
         (UIHost.GetUI("SaveStart") as ParameterButton).Clicked += SaveStart_Clicked;
@@ -58,15 +64,15 @@ public class SaveData : MonoBehaviour {
         this.FpsAd = new FPSAdjuster.FPSAdjuster();
         this.FpsAd.Fps = 30;
         this.FpsAd.Start();
-        
+        //Debug.Log("start1");
+
     }
 
-    private void ChooseFile_Clicked(object sender, EventArgs e)
+    private void ChooseFolder_Clicked(object sender, EventArgs e)
     {
-
-        OpenFileDialog.OpenFileDialog.Save(ref Filename);
-        Debug.Log(Filename);
-        
+        filename = "";
+        OpenFileDialog.OpenFileDialog.Save(ref filename);
+        Debug.Log(filename);
     }
 
     private void OpenFileChoose_ValueChanged(object sender, System.EventArgs e)
@@ -80,18 +86,20 @@ public class SaveData : MonoBehaviour {
     }
 
     // Update is called once per frame
-    void Update () {
+    void Update()
+    {
 
-        
+
     }
 
     void SaveDepth()
     {
-        unsafe {
+        unsafe
+        {
             try
             {
                 this._savedDepthBuffer = this.kinect.m_SaveDepth;
-               // Mat mat = new Mat(new Size(512, 424), MatType.CV_16U);
+                // Mat mat = new Mat(new Size(512, 424), MatType.CV_16U);
                 while (true)
                 {
                     this.FpsAd.Adjust();
@@ -102,14 +110,14 @@ public class SaveData : MonoBehaviour {
                     //}
 
                     //byte[] data = mat.ToBytes(".png");
-                    
+
                     datetime = DateTime.Now;
                     timestump = datetime.TimeOfDay;
                     //Debug.Log(framecount);
                     writer.Write(timestump.ToString());
                     writer.Write(_savedDepthBuffer.Length);
 
-                    for (int i = 0;i<_savedDepthBuffer.Length;i++)
+                    for (int i = 0; i < _savedDepthBuffer.Length; i++)
                     {
                         writer.Write(_savedDepthBuffer[i]);
                     }
@@ -144,9 +152,9 @@ public class SaveData : MonoBehaviour {
             thread.Abort();
 
         }
-        if (Filename != null)
+        if (FolderPath != null)
         {
-            Filename = null;
+            FolderPath = null;
         }
     }
 }
