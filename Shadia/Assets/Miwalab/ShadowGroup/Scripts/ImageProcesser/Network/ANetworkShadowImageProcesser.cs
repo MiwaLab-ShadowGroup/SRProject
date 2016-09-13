9b163d268d69953e6524074f7553ee1073385bf3
YusukeKajita <kajita1002@gmail.com>
2016-09-14 00:30:15 +0900

    test

--- Shadia/Assets/Miwalab/ShadowGroup/Scripts/ImageProcesser/Network/ANetworkShadowImageProcesser.cs
+++ Shadia/Assets/Miwalab/ShadowGroup/Scripts/ImageProcesser/Network/ANetworkShadowImageProcesser.cs
@@ -7,14 +7,7 @@ namespace Miwalab.ShadowGroup.ImageProcesser.Network
 {
     public abstract class ANetworkShadowImageProcesser : AShadowImageProcesser
     {
-        private Miwalab.ShadowGroup.Thread.ThreadHost _tHost;
-        private Miwalab.ShadowGroup.Network.NetworkHost _nHost;
-        public void SetupHosts()
-        {
-            _nHost = ShadowGroup.Network.NetworkHost.GetInstance();
-            _tHost = ShadowGroup.Thread.ThreadHost.GetInstance();
-            
-        }
+        
 
     }
 }
---
+++ Shadia/Assets/Miwalab/ShadowGroup/Scripts/ImageProcesser/Network/RemoteShadowImageManager.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using Miwalab.ShadowGroup.Network;
+using Miwalab.ShadowGroup.Thread;
+
+public class RemoteShadowImageManager : MonoBehaviour
+{
+    NetworkHost _nHost;
+    ThreadHost _tHost;
+
+    int _myPort;
+    int _remotePort;
+
+    // Use this for initialization
+    void Start()
+    {
+        _nHost = NetworkHost.GetInstance();
+        _tHost = ThreadHost.GetInstance();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
