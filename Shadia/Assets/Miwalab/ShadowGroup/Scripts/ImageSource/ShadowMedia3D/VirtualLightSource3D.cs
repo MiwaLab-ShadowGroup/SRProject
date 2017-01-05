cf092b681b2ae01a2acc8ba0b75362c19327aa64
YusukeKajita <kajita1002@gmail.com>
2017-01-05 23:40:40 +0900

    三次元モデルが画像として取り込まれる歴史的瞬間

--- Shadia/Assets/Miwalab/ShadowGroup/Scripts/ImageSource/ShadowMedia3D/VirtualLightSource3D.cs
+++ Shadia/Assets/Miwalab/ShadowGroup/Scripts/ImageSource/ShadowMedia3D/VirtualLightSource3D.cs
@@ -17,27 +17,41 @@ namespace Miwalab.ShadowGroup.ImageSource.ShadowMedia3D
         void Start()
         {
             _camera = GetComponent<Camera>();
-            (ShadowMediaUIHost.GetUI("Kinect_pos_x") as ParameterSlider).ValueChanged += VirtualLightSource3D_pos_x_ValueChanged;
-            (ShadowMediaUIHost.GetUI("Kinect_pos_y") as ParameterSlider).ValueChanged += VirtualLightSource3D_pos_y_ValueChanged;
-            (ShadowMediaUIHost.GetUI("Kinect_pos_z") as ParameterSlider).ValueChanged += VirtualLightSource3D_pos_z_ValueChanged;
+            (ShadowMediaUIHost.GetUI("kinect_height") as ParameterSlider).ValueChanged += VirtualLightSource3D_height_ValueChanged;
+            (ShadowMediaUIHost.GetUI("kinect_angle") as ParameterSlider).ValueChanged += VirtualLightSource3D_angle_ValueChanged;
+            (ShadowMediaUIHost.GetUI("kinect_radius") as ParameterSlider).ValueChanged += VirtualLightSource3D_radius_ValueChanged;
             (ShadowMediaUIHost.GetUI("Kinect_rot_x") as ParameterSlider).ValueChanged += VirtualLightSource3D_rot_x_ValueChanged;
             (ShadowMediaUIHost.GetUI("Kinect_rot_y") as ParameterSlider).ValueChanged += VirtualLightSource3D_rot_y_ValueChanged;
             (ShadowMediaUIHost.GetUI("Kinect_light_r") as ParameterSlider).ValueChanged += VirtualLightSource3D_light_r_ValueChanged;
             (ShadowMediaUIHost.GetUI("Kinect_LightMode") as ParameterDropdown).ValueChanged += VirtualLightSource3D_LightModeChanged;
             (ShadowMediaUIHost.GetUI("Kinect_ViewRange") as ParameterSlider).ValueChanged += VirtualLightSource3D_ViewRangeChanged;
 
-            (ShadowMediaUIHost.GetUI("Kinect_pos_x") as ParameterSlider).ValueUpdate();
-            (ShadowMediaUIHost.GetUI("Kinect_pos_y") as ParameterSlider).ValueUpdate();
-            (ShadowMediaUIHost.GetUI("Kinect_pos_z") as ParameterSlider).ValueUpdate();
+            (ShadowMediaUIHost.GetUI("kinect_height") as ParameterSlider).ValueUpdate();
+            (ShadowMediaUIHost.GetUI("kinect_angle") as ParameterSlider).ValueUpdate();
+            (ShadowMediaUIHost.GetUI("kinect_radius") as ParameterSlider).ValueUpdate();
             (ShadowMediaUIHost.GetUI("Kinect_rot_x") as ParameterSlider).ValueUpdate();
             (ShadowMediaUIHost.GetUI("Kinect_rot_y") as ParameterSlider).ValueUpdate();
 
         }
 
+        private void VirtualLightSource3D_radius_ValueChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void VirtualLightSource3D_angle_ValueChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void VirtualLightSource3D_height_ValueChanged(object sender, EventArgs e)
+        {
+            var pos = this.gameObject.transform.position;
+            this.gameObject.transform.position = new Vector3(pos.x, (e as ParameterSlider.ChangedValue).Value, pos.z);
+        }
+
         private void VirtualLightSource3D_ViewRangeChanged(object sender, EventArgs e)
         {
             float t = (e as ParameterSlider.ChangedValue).Value;
-            this._camera.fieldOfView = Mathf.Atan(t) * 180 / Mathf.PI;
+            this._camera.fieldOfView = Mathf.Atan(t) * 180 / Mathf.PI * 2f;
         }
 
         private void VirtualLightSource3D_LightModeChanged(object sender, EventArgs e)
@@ -50,34 +64,17 @@ namespace Miwalab.ShadowGroup.ImageSource.ShadowMedia3D
 
         private void VirtualLightSource3D_rot_y_ValueChanged(object sender, EventArgs e)
         {
-            _kinectRotation_rx = (e as ParameterSlider.ChangedValue).Value;
+            _kinectRotation_ry = -(e as ParameterSlider.ChangedValue).Value;
             this.gameObject.transform.rotation = Quaternion.Euler(_kinectRotation_rx, _kinectRotation_ry, 0);
         }
 
         private void VirtualLightSource3D_rot_x_ValueChanged(object sender, EventArgs e)
         {
-            _kinectRotation_rx = (e as ParameterSlider.ChangedValue).Value;
+            _kinectRotation_rx = -(e as ParameterSlider.ChangedValue).Value;
 
             this.gameObject.transform.rotation = Quaternion.Euler(_kinectRotation_rx, _kinectRotation_ry, 0);
         }
-
-        private void VirtualLightSource3D_pos_z_ValueChanged(object sender, EventArgs e)
-        {
-            var pos = this.gameObject.transform.position;
-            this.gameObject.transform.position = new Vector3(pos.x, pos.y, (e as ParameterSlider.ChangedValue).Value);
-        }
-
-        private void VirtualLightSource3D_pos_y_ValueChanged(object sender, EventArgs e)
-        {
-            var pos = this.gameObject.transform.position;
-            this.gameObject.transform.position = new Vector3(pos.x, (e as ParameterSlider.ChangedValue).Value, pos.z);
-        }
-
-        private void VirtualLightSource3D_pos_x_ValueChanged(object sender, EventArgs e)
-        {
-            var pos = this.gameObject.transform.position;
-            this.gameObject.transform.position = new Vector3((e as ParameterSlider.ChangedValue).Value, pos.y, pos.z);
-        }
+        
 
         void Update()
         {
