a0d5fe186d7f7ad089d8ed1ef1fce429babc4bdd
Yyamakawa <yy816-b.t.a@ruri.waseda.jp>
2017-01-06 20:32:50 +0900

    CIPC通信部分の作成途中

---
+++ Shadia/Assets/Operation/Networkoperation.cs
@@ -0,0 +1,27 @@
+﻿using Miwalab.ShadowGroup.Network;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class NetworkOperation : Miwalab.ShadowGroup.Operation.AOperationCommand
+{
+
+    public List<Object3DOperation> object3DOpe = new List<Object3DOperation>();
+
+    MemoryStream ms;
+
+    public byte[] data;
+   
+    public void ChangeToBinary()
+    {
+        ms = new MemoryStream();
+
+        System.Xml.Serialization.XmlSerializer serializer = new System.Xml.Serialization.XmlSerializer(typeof(NetworkOperation));
+
+        serializer.Serialize(ms,this);
+
+        data = ms.ToArray();
+    }
+
+}
