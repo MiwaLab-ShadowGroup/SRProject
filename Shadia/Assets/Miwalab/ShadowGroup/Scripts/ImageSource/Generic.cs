690cfacf9cdce476ef4f41081f2f2bddbb8c502a
YusukeKajita <kajita1002@gmail.com>
2017-01-06 13:25:30 +0900

    BlendModeを追加

---
+++ Shadia/Assets/Miwalab/ShadowGroup/Scripts/ImageSource/Generic.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Miwalab.ShadowGroup.ImageSource
+{
+    public enum BlendMode
+    {
+        Normal,
+        Add,
+        Substract,
+        Multiply,
+        other
+    }
+}
